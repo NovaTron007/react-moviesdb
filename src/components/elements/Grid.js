a15c1c0e603a5e09f7ccf143ee5354378898519e
DwayneMan <dwayne.man@gmail.com>
2020-08-15 23:37:44 +0800

    Passing props from  MovieThumb component to display grid images. Passing props from Home component to Grid component. Using styled components in Grid and MovieThumb component.

--- src/components/elements/Grid.js
+++ src/components/elements/Grid.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { StyledGrid, StyledGridContent } from "../styles/StyledGrid"; // get styles from StyledGrid
 
-const Grid = () => <div>Grid</div>;
+// receive props
+// children will access child components nested
+// inside grid component all in Home.js
+const Grid = ({ header, children }) => (
+  <StyledGrid>
+    <h1>{header}</h1>
+    <StyledGridContent>{children}</StyledGridContent>
+  </StyledGrid>
+);
 
 export default Grid;
